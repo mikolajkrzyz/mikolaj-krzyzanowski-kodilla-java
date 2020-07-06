c495eaeb5fe289adea2f09ef7f2e3ffd0f6c893f
Mikołaj Krzyżanowski <mikolaj@MacBook-Pro-Mikoaj.local>
2020-07-06 19:50:13 +0200

    sub added

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,7 +1,11 @@
 package com.kodilla;
 
 public class Calculator {
-    public double add(double a, double b){
-        return a+b;
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
     }
 }
