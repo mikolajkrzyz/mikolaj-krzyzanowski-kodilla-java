269eb8ef51a38136558ad4477059c46a24ef405a
Mikołaj Krzyżanowski <mikolaj@MacBook-Pro-Mikoaj.local>
2020-07-06 19:45:40 +0200

    calculator added

---
+++ src/main/java/com/kodilla/App.java
@@ -0,0 +1,8 @@
+package com.kodilla;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(3,8));
+    }
+}
---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,7 @@
+package com.kodilla;
+
+public class Calculator {
+    public double add(double a, double b){
+        return a+b;
+    }
+}
