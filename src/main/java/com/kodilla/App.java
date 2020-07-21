328c24c2e92e7d6c761639fa82038da5fa1b6e7a
Mikołaj Krzyżanowski <mikolaj@MacBook-Pro-Mikoaj.local>
2020-07-21 22:08:16 +0200

    firstmainclass

--- src/main/java/com/kodilla/App.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla;
-
-public class App {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println(calc.add(3,8));
-    }
-}
