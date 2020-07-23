f00be613c32320d6331ba27245b2f23439682423
Mikołaj Krzyżanowski <mikolaj@MacBook-Pro-Mikoaj.local>
2020-07-23 20:26:42 +0200

    testkalkulator

--- src/main/java/com/kodilla/testing/TestingMain.java
+++ src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,4 +1,12 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.SimpleCalculator;
+
 
 public class TestingMain {
+    public static void main(String[] args) {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+
+        System.out.println(simpleCalculator.add(3,5));
+        System.out.println(simpleCalculator.subtract(12,4));
+    }
 }
---
+++ src/main/java/com/kodilla/testing/calculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class SimpleCalculator {
+    public double add(double a, double b){
+            return a+b;
+    }
+        public double subtract(double a, double b) {
+            return a-b;
+         }
+}
+
