fafc2cb76e9974ec0b0e425bf36b2c7c0b829356
Mikołaj Krzyżanowski <mikolaj@MacBook-Pro-Mikoaj.local>
2020-07-25 21:01:46 +0200

    CalculatorSimple

--- kodilla-testing/src/main/java/SimpleCalculator/CalculatorTest.java
+++ kodilla-testing/src/main/java/SimpleCalculator/CalculatorTest.java
@@ -3,9 +3,20 @@ import SimpleCalculator.FirstCalculator;
 public class CalculatorTest {
     public static void main(String[] args) {
         FirstCalculator simpleCalculator = new FirstCalculator();
-
-        System.out.println(simpleCalculator.add(4, 5));
-        System.out.println(simpleCalculator.subtract(12, 4));
+        double sum = simpleCalculator.add(4, 5);
+        if (sum == 9) {
+            System.out.println("Super");
+        } else {
+            System.out.println("Nie udało się");
+        }
+        FirstCalculator simpleCalculator2 = new FirstCalculator();
+        double summary = simpleCalculator2.add(12, 4);
+        if (summary == 8) {
+            System.out.println("brawo");
+        } else {
+            System.out.println("coś poszło nie tak");
+        }
     }
-
 }
+
+
